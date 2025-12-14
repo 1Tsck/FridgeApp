1caf76fbb5f7c3e7768c5c62827dbe964ae8d57e
1Tsck <ju.jaworski@gmail.com>
2025-12-14 19:58:14 +0100

    dependencies update

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,8 @@
-from backend import crud
-from backend import dependencies
-
-#from dependencies import get_current_user, require_admin
+from . import crud
+from . dependencies import get_current_user, require_admin
 from fastapi import File, UploadFile
 from datetime import datetime, timezone, timedelta
-import os
-import uvicorn
+
 from fastapi import FastAPI, Request, Query, Depends, Form, HTTPException, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
@@ -19,11 +16,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIR = BASE_DIR / "frontend" / "static"
 TEMPLATES_DIR = BASE_DIR / "frontend" / "templates"
 
-port = int(os.environ.get("PORT", 8080))  # Railway sets PORT automatically
-
-if __name__ == "__main__":
-    uvicorn.run("backend.main:app", host="0.0.0.0", port=port)
-
 app = FastAPI()
 app.mount('/static', StaticFiles(directory=STATIC_DIR), name='static')
 templates = Jinja2Templates(directory=TEMPLATES_DIR)
@@ -34,7 +26,7 @@ async def favicon():
     return FileResponse(STATIC_DIR / "favicon.ico")
 
 @app.get('/admin', response_class=HTMLResponse)
-async def admin_page(request: Request, filter: str = Query("", alias="filter"), msg: str = Query("", alias="msg"), user=Depends(dependencies.require_admin)):
+async def admin_page(request: Request, filter: str = Query("", alias="filter"), msg: str = Query("", alias="msg"), user=Depends(require_admin)):
     types = crud.list_item_types(filter)
     return templates.TemplateResponse('admin.html', {'request': request, 'types': types, 'filter': filter, 'msg': msg})
 
@@ -67,62 +59,62 @@ async def stats(request: Request, filter: str = Query("", alias="filter"), start
     return templates.TemplateResponse('stats.html', {'request': request,'stats': stats, 'change_log': change_log, 'filter': filter, 'default_start': default_start, 'default_end': default_end, 'msg': msg})
 
 @app.post('/admin/item-type/create')
-async def create_item_type(add_item_name: str = Form(...), add_item_desc: str = Form(...), user=Depends(dependencies.require_admin)):
+async def create_item_type(add_item_name: str = Form(...), add_item_desc: str = Form(...), user=Depends(require_admin)):
     err = crud.add_item_type(add_item_name, add_item_desc)
     url = prepareUrl('/admin', err, 'Item type added successfully!')
     return RedirectResponse(url, status_code=303)
 
 @app.post('/fridge/item/create')
-async def add_fridge_item(add_item_item_type_id: str = Form(...), add_item_quantity: float = Form(...), add_item_unit: str = Form(...), add_photo: UploadFile | None = File(None), add_expiry_date:str | None = Form(None), user=Depends(dependencies.get_current_user)):
+async def add_fridge_item(add_item_item_type_id: str = Form(...), add_item_quantity: float = Form(...), add_item_unit: str = Form(...), add_photo: UploadFile | None = File(None), add_expiry_date:str | None = Form(None), user=Depends(get_current_user)):
     err = crud.add_fridge_item(add_item_item_type_id, add_item_quantity, add_item_unit, user.get('email'), add_photo, add_expiry_date)
     url = prepareUrl('/fridge', err, 'Item successfully added to the fridge!')
     return RedirectResponse(url, status_code=303)
 
 @app.post('/cart/item/create')
-async def add_cart_item(add_item_item_type_id: str = Form(...), add_item_quantity: float = Form(...), add_item_unit: str = Form(...), user=Depends(dependencies.get_current_user)):
+async def add_cart_item(add_item_item_type_id: str = Form(...), add_item_quantity: float = Form(...), add_item_unit: str = Form(...), user=Depends(get_current_user)):
     err = crud.add_cart_item(add_item_item_type_id, add_item_quantity, add_item_unit, user.get('email'))
     url = prepareUrl('/cart', err, 'Item successfully added to the shoping cart!')
     return RedirectResponse(url, status_code=303)
 
 @app.post('/fridge/item/addtocart')
-async def add_to_cart(item_type_id: str = Form(...), quantity: float = Form(...), unit: str = Form(...), user=Depends(dependencies.get_current_user)):
+async def add_to_cart(item_type_id: str = Form(...), quantity: float = Form(...), unit: str = Form(...), user=Depends(get_current_user)):
     err = crud.add_cart_item(item_type_id, quantity, unit, user.get('email'))
     url = prepareUrl('/fridge', err, 'Item successfully added to the shoping cart!')
     return RedirectResponse(url, status_code=303)
 
 @app.post('/admin/item-type/update')
-async def update_item_type(item_type_id: str = Form(...), name: str = Form(...), description: str = Form(''), user=Depends(dependencies.require_admin)):
+async def update_item_type(item_type_id: str = Form(...), name: str = Form(...), description: str = Form(''), user=Depends(require_admin)):
     err = crud.update_item_type(item_type_id, name, description)
     url = prepareUrl('/admin', err, 'Item type modified successfully!')
     return RedirectResponse(url, status_code=303)
 
 @app.post('/fridge/item/update')
-async def update_fridge_item(item_id: str = Form(...), type_name: str = Form(...), quantity: float = Form(...), unit: str = Form(...), photo: UploadFile | None = File(None), expiry_date: str | None = Form(None), user=Depends(dependencies.get_current_user)):
+async def update_fridge_item(item_id: str = Form(...), type_name: str = Form(...), quantity: float = Form(...), unit: str = Form(...), photo: UploadFile | None = File(None), expiry_date: str | None = Form(None), user=Depends(get_current_user)):
     
     err = crud.update_fridge_item(item_id, quantity, unit,user.get('email'), type_name, photo, expiry_date)
     url = prepareUrl('/fridge', err, 'Item successfully updated in the fridge!')
     return RedirectResponse(url, status_code=303)
 
 @app.post('/cart/item/update')
-async def update_cart_item(cart_id: str = Form(...), quantity: float = Form(...), unit: str = Form(...), user=Depends(dependencies.get_current_user)):
+async def update_cart_item(cart_id: str = Form(...), quantity: float = Form(...), unit: str = Form(...), user=Depends(get_current_user)):
     err = crud.update_cart_item(cart_id, quantity, unit, user.get('email'))
     url = prepareUrl('/cart', err, 'Item successfully updated in the shoping cart!')
     return RedirectResponse(url, status_code=303)
 
 @app.post("/admin/item-type/delete")
-async def delete_item_type(item_type_id: str = Form(...), user=Depends(dependencies.require_admin)):
+async def delete_item_type(item_type_id: str = Form(...), user=Depends(require_admin)):
     err = crud.delete_item_type(item_type_id)
     url = prepareUrl('/admin', err, 'Item type deleted successfully!')
     return RedirectResponse(url, status_code=303)
 
 @app.post("/fridge/item/delete")
-async def delete_fridge_item(type_name: str = Form(...), item_id: str = Form(...), user=Depends(dependencies.get_current_user)):
+async def delete_fridge_item(type_name: str = Form(...), item_id: str = Form(...), user=Depends(get_current_user)):
     err = crud.delete_fridge_item(item_id, user.get('email'), type_name)
     url = prepareUrl('/fridge', err, 'Item successfully removed from the fridge!')
     return RedirectResponse(url, status_code=303)
 
 @app.post("/cart/item/delete")
-async def delete_cart_item(cart_id: str = Form(...), user=Depends(dependencies.get_current_user)):
+async def delete_cart_item(cart_id: str = Form(...), user=Depends(get_current_user)):
     err = crud.delete_cart_item(cart_id)
     url = prepareUrl('/cart', err, 'Item successfully removed from the cart!')
     return RedirectResponse(url, status_code=303)
@@ -145,7 +137,7 @@ async def login_page(request: Request):
 async def add_user_to_request(request: Request, call_next):
     try:
         # Try to get user from token / session
-        user = await dependencies.get_current_user(request)
+        user = await get_current_user(request)
     except Exception:
         user = None  # not logged in or token invalid
     request.state.user = user
